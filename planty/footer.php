29b15948c2cd6b60e7a062c5d7ee64cf714ae62b
Loïc Dupin <loic.dupin1@gmail.com>
2023-06-29 18:28:10 +0200

    First commit

---
+++ planty/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+?>
+        <footer id="site-footer" class="header-footer-group">
+            <img src="http://localhost/Planty/wp-content/uploads/2023/06/Canette.png" alt=" Images représentant une ligne de canettes Planty" class="footer-position">
+            <a href="#" class="noborder-color">Mentions légales</a>
+        </footer><!-- #site-footer -->
+
+      <?php wp_footer(); ?>
+
+   </body>
+</html>
---
+++ planty/functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+add_action ('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+}
+/*HOOKS*/
+add_filter( 'wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2 );
+function add_extra_item_to_nav_menu( $items, $args ) {
+    if (is_user_logged_in()) {
+        $items .= '<li><a href="http://localhost/Planty/wp-admin/index.php" class="admin-position">Admin</a></li>';
+    }
+    return $items;
+}
